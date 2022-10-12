ef71ef167349fccee9489f8cf33222ed406c5244
Tilio77 <tiliobolso1@gmail.com>
2022-10-12 19:20:19 -0300

    Commit inicial

---
+++ core/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import HomeView, UserProductListView, ProductUpdateView, ProductDetailView, CreateCheckoutSessionView
+from .views import UserLibraryView
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('users/', include('accounts.urls', namespace='users')),
+    path('create-checkout-session/<slug>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('', HomeView.as_view(), name='home'),
+    path('library/<username>/', UserLibraryView.as_view(), name='library'),
+    
+    path('products/', UserProductListView.as_view(), name='product-list'),
+    path('products/<slug>/', ProductDetailView.as_view(), name='product-detail'),
+
+    path('products/<slug>/update/', ProductUpdateView.as_view(), name='product-update'),
+
+    path('marketplace/', include('marketplace.urls', namespace='marketplace'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
